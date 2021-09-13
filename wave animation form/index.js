0a99bb010785869a69375d06a9fe41ad65613262
Dylan Owens <90014103+DylanOwens-Dev@users.noreply.github.com>
2021-09-13 10:07:13 -0400

    init

---
+++ wave animation form/index.js
@@ -0,0 +1,5 @@
+var labels = document.querySelectorAll('.form-control label')
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText.split('').map((letter, idx)=>`<span style="transition-delay:${idx * 75}ms">${letter}</span>`).join('')
+})
